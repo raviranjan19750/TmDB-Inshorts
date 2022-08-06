c769a9c7ec9929f7523527211077adc780bb3375
raviranjan19750 <ranjan.ranjan19750@gmail.com>
2022-08-06 23:13:49 +0530

    github link updated

--- app/src/main/java/com/example/tmdb_isnhorts/View/AboutActivity.java
+++ app/src/main/java/com/example/tmdb_isnhorts/View/AboutActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +16,9 @@ import com.example.tmdb_isnhorts.R;
 public class AboutActivity extends AppCompatActivity {
 
     ImageView githubImageView;
-    TextView githubUserNameTextView;
-    String url = "https://github.com/raviranjan19750/AwesomeProject";
+    TextView githubUserNameTextView, clickTextView;
+    LinearLayout githubLinkLinearLayout;
+    String url = "https://github.com/raviranjan19750/TmDB-Inshorts";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,46 +27,44 @@ public class AboutActivity extends AppCompatActivity {
 
         githubImageView = findViewById(R.id.githubImageView);
         githubUserNameTextView = findViewById(R.id.githubUserNameTextView);
+        clickTextView = findViewById(R.id.clickTextView);
+        githubLinkLinearLayout = findViewById(R.id.githubLinkLinearLayout);
 
         getSupportActionBar().setTitle("TmDb-Inshorts");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        githubImageView.setOnClickListener(new View.OnClickListener() {
+        clickTextView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
 
-                Uri uri = Uri.parse(url);
-                Intent i = new Intent(Intent.ACTION_VIEW, uri);
-
-                try {
-                    startActivity(i);
-                } catch (ActivityNotFoundException e) {
-
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(url)));
-                }
+                openGithubLink();
 
             }
         });
+       githubLinkLinearLayout.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
 
-        githubUserNameTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+               openGithubLink();
 
-                Uri uri = Uri.parse(url);
-                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+           }
+       });
+    }
 
-                try {
-                    startActivity(i);
-                } catch (ActivityNotFoundException e) {
+    private void openGithubLink() {
 
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(url)));
-                }
+        Uri uri = Uri.parse(url);
+        Intent i = new Intent(Intent.ACTION_VIEW, uri);
 
-            }
-        });
+        try {
+            startActivity(i);
+        } catch (ActivityNotFoundException e) {
+
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(url)));
+        }
+
     }
 
 
